8c6436c7fa1e72689aef5381ec3b4b3774abc753
Ludumo <ludumo.toni@outlook.com>
2022-09-23 12:24:47 +0200

    3. testing contracts

---
+++ test/Token.js
@@ -0,0 +1,38 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { before } = require("lodash");
+
+const tokens = (n) => {
+    return ethers.utils.parseUnits(n.toString(), "ether")
+}
+
+describe("Token", () => {
+    let token
+
+    beforeEach(async() =>{
+        const Token = await ethers.getContractFactory("Token")
+        token = await Token.deploy("Toni Token", "TONI", "1000000")
+    })
+
+    describe("Deployment", () => {
+        const name = "Toni Token"
+        const symbol = "TONI"
+        const decimals = "18"
+        const totalSupply = "1000000"
+
+        it("has correct name", async () =>{
+            expect(await token.name()).to.equal(name)
+        })
+    
+        it("has correct symbol", async () =>{
+            expect(await token.symbol()).to.equal(symbol)
+        })
+        it("has correct decimals", async () =>{
+            expect(await token.decimals()).to.equal(decimals)
+        })
+        it("has correct total Supply", async () =>{
+            expect(await token.totalSupply()).to.equal(tokens(totalSupply))
+        })
+    
+    }) 
+})
