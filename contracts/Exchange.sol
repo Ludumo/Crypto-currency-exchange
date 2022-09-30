cd8ea54110df7d90d0ac85fd9e696eb2016259a3
Ludumo <ludumo.toni@outlook.com>
2022-09-30 14:52:36 +0200

    10. Withdraw Tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -10,6 +10,12 @@ contract Exchange {
     mapping(address => mapping(address => uint256)) public tokens;
 
     event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Withdraw(
+        address token, 
+        address user, 
+        uint256 amount, 
+        uint256 balance
+    );
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
@@ -20,17 +26,32 @@ contract Exchange {
     function depositToken(address _token, uint256 _amount) public {
          //Transfer tokens to exchange
          require(Token(_token).transferFrom(msg.sender, address(this), _amount));
-         
+
          //Update user balance
          tokens[_token][msg.sender] = tokens[_token][msg.sender] + _amount;
          //Emit an event
          emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
+
+    function withdrawToken(address _token, uint256 _amount) public {
+        //Ensure user has enough tokens to withdraw
+        require(tokens[_token][msg.sender] >= _amount);        
+
+        //Transfer tokens to user
+        Token(_token).transfer(msg.sender, _amount);
+
+        // Update user balance
+        tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
+
+        //Emit event
+        emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
     // Check Balances
     function balanceOf(address _token, address _user)
-    public
-    view
-    returns (uint256) 
+        public
+        view
+        returns (uint256) 
     {
         return tokens[_token][_user];
     }
